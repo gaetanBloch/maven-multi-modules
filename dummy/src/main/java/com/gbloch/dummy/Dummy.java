9bb39a2617ac0a237637079ccbb9e5ea294ad1c7
gbloch <gaetan.bloch@gmail.com>
2021-02-26 18:59:26 +0100

    Created a dummy child module using IntelliJ IDEA

---
+++ dummy/src/main/java/com/gbloch/dummy/Dummy.java
@@ -0,0 +1,16 @@
+package com.gbloch.dummy;
+
+/**
+ * Dummy Class
+ * <br>
+ * Created on 26/02/2021
+ *
+ * @author gbloch
+ * @version 1.0
+ * @since 1.0
+ */
+public class Dummy {
+    public String dummy() {
+        return "I'm useless";
+    }
+}
---
+++ dummy/src/test/java/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * <br>
+ * Created on 26/02/2021
+ *
+ * @author gbloch
+ * @version 1.0
+ * @since 1.0
+ */
